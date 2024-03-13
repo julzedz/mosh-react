20ed2e625713660be68984d8c36ad7911f138c82
julzedz <edoziechukwuma@gmail.com>
2024-03-13 17:49:07 +0100

    Add Paris to ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,13 +4,14 @@ function ListGroup() {
     'San Francisco',
     'Tokyo',
     'London',
+    'Paris',
   ]
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.map((i) => (
-          <li>{i}</li>
+        {items.map((i, index) => (
+          <li key={i} onClick={() => console.log(i, index)} className="list-group-item">{i}</li>
         ))}
       </ul>
     </>
