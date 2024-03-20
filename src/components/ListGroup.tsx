c7830779c8ef29ef4425be56ba5257e1816d6a83
julzedz <edoziechukwuma@gmail.com>
2024-03-20 08:58:01 +0100

    Refactor ListGroup component to add item selection

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+
 function ListGroup() {
-  const items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris',
-  ]
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [ selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((i, index) => (
-          <li key={i} onClick={() => console.log(i, index)} className="list-group-item">{i}</li>
+          <li
+            key={i}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {i}
+          </li>
         ))}
       </ul>
     </>
