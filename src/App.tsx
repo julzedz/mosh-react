e89c75d102dace0a0a92bf8a1c84034777de011c
julzedz <edoziechukwuma@gmail.com>
2024-03-21 08:07:39 +0100

    Passing Functions via pprops

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
 
-  return <div><ListGroup items={items} heading='Cities' /></div>
+  return <div><ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem} /></div>
 }
 export default App;
