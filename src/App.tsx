2884f92331ff63ac857ba73726f86eac9c5d0119
julzedz <edoziechukwuma@gmail.com>
2024-03-25 10:48:02 +0100

    Add Alert component and implement functionality to show and hide the alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>Me Button</Button>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button onClick={() => setAlertVisibility(true)}>Me Button</Button>
     </div>
   );
 }
