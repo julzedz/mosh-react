02e0ffde526fe51291ef0f344198cb00ea2f2f5d
julzedz <edoziechukwuma@gmail.com>
2024-03-29 10:46:56 +0100

    Refactor import statements and add Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod accusamus ipsum sed eius consequatur voluptatibus illum earum possimus maiores laborum porro atque minus temporibus ullam debitis molestias unde soluta, ab praesentium. Sapiente inventore, ab, libero dicta doloribus voluptates culpa alias, temporibus laborum labore minus quia repellendus fuga cum aliquid explicabo fugit sunt expedita perspiciatis fugiat doloremque velit dignissimos quas ratione. Asperiores doloribus earum omnis nam dolorum ipsum, cupiditate labore magni, pariatur voluptatibus quisquam, dignissimos illum hic enim fugit saepe molestiae et quibusdam facere! Ab consequatur pariatur optio voluptatibus tempore voluptatem exercitationem vel quae! Quos, quo. Fugiat, maiores rem? Ex praesentium optio ipsa illum quisquam repellendus, distinctio amet molestiae esse facilis magni nostrum iure qui dolore ut alias assumenda tenetur nisi? Numquam, aspernatur recusandae. Corrupti at molestiae vel facere! Nostrum quos explicabo culpa cupiditate quisquam laboriosam incidunt a, enim, distinctio dolor aperiam impedit quas voluptates iure doloribus provident dolorem? Molestiae, accusamus.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
